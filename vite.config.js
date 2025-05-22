eb82d00443dba1e7fcd6b53102f7b0fbc1bb1382
Eatomo <abc1158000@gmail.com>
2025-05-22 11:31:47 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+<<<<<<< HEAD
+  base: '/vue/' ,
+=======
+  base: '/vue/',
+>>>>>>> f01716bd2d4981a4795185e9c172f055e002b076
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
