7ba8a67f336cc8d75f58a31b9792989ef499b8f6
Eatomo <abc1158000@gmail.com>
2025-05-22 12:17:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/vue/' ,
-=======
+ HEAD
   base: '/vue/',
->>>>>>> f01716bd2d4981a4795185e9c172f055e002b076
+f01716bd2d4981a4795185e9c172f055e002b076
   plugins: [vue()],
   resolve: {
     alias: {
